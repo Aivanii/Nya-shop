af68b26c0081249441b232521ef4d3624efd0bcd
Aivanii <168092301+Aivanii@users.noreply.github.com>
2024-06-04 12:08:26 +0700

    Update theme_changer.js

--- js/theme_changer.js
+++ js/theme_changer.js
@@ -12,16 +12,27 @@ function theme_changing() {
     for (let i = 0; i < a_objects.length; i++) {
         a_objects[i].classList.toggle("dark");
     }
+    document.getElementById("logo").classList.toggle("dark");
 
     if(actual_theme === "light"){
         document.documentElement.style.setProperty('--bs-border-color', dark_color);
         document.querySelector('.navbar').style.setProperty('--bs-navbar-toggler-border-color', dark_color);
         document.getElementById("navbar-toggler-icon").style.backgroundImage = "url('../img/toggler-icon-bg-dark.svg')";
+
+        let logo_img = getComputedStyle(document.documentElement).getPropertyValue('--dark-logo-img').replace(/"/g, '');;
+        document.getElementById("logo_img").src = logo_img;
+
+        document.documentElement.style.setProperty('--actual-theme', "dark");
         actual_theme = "dark";
     }else{
         document.documentElement.style.setProperty('--bs-border-color', light_color);
         document.querySelector('.navbar').style.setProperty('--bs-navbar-toggler-border-color', light_color);
         document.getElementById("navbar-toggler-icon").style.backgroundImage = "url('../img/toggler-icon-bg.svg')";
+
+        let logo_img = getComputedStyle(document.documentElement).getPropertyValue('--light-logo-img').replace(/"/g, '');;
+        document.getElementById("logo_img").src = logo_img;
+
+        document.documentElement.style.setProperty('--actual-theme', "light");
         actual_theme = "light";
     }
 }
