59d598b5ee55fab8e4666920c15b0cf464aa99e5
Aivanii <aivanellok@gmail.com>
2024-07-10 15:43:20 +0700

    v-0.4.3

--- css/js/theme_changer.js
+++ css/js/theme_changer.js
@@ -1,36 +1,47 @@
 document.getElementById("color_change_button").addEventListener("click", theme_changing);
 
-const dark_color = "#17191e"
-const light_color = "#7e42ff"
+const dark_color = "#202034";
+const light_color = "#262641";
+
+const white_color = "#fff";
+
+
+const white_text_color = "#ffffff8c";
+const dark_text_color = "#0000008c";
 
 let actual_theme = "light";
 
 function theme_changing() {
-    document.body.classList.toggle("dark");
-    document.getElementById("navbar").classList.toggle("dark");
-    let a_objects = document.getElementsByClassName("nav-link");
-    for (let i = 0; i < a_objects.length; i++) {
-        a_objects[i].classList.toggle("dark");
-    }
-    document.getElementById("logo").classList.toggle("dark");
 
     if(actual_theme === "light"){
-        document.documentElement.style.setProperty('--bs-border-color', dark_color);
-        document.querySelector('.navbar').style.setProperty('--bs-navbar-toggler-border-color', dark_color);
         document.getElementById("navbar-toggler-icon").style.backgroundImage = "url('../img/toggler-icon-bg-dark.svg')";
 
-        let logo_img = getComputedStyle(document.documentElement).getPropertyValue('--dark-logo-img').replace(/"/g, '');;
+        let logo_img = getComputedStyle(document.documentElement).getPropertyValue('--dark-logo-img').replace(/"/g, '');
         document.getElementById("logo_img").src = logo_img;
+
+        document.body.style.background = "#262641";
+
+        document.documentElement.style.setProperty('--actual-hovered-links-color', "white");
+        document.documentElement.style.setProperty('--actual-color-one', light_color);
+        document.documentElement.style.setProperty('--actual-color-two', dark_color);
+        document.documentElement.style.setProperty('--actual-color-three', dark_color);
+        document.documentElement.style.setProperty('--actual-non-active-links-color', white_text_color);
 
         document.documentElement.style.setProperty('--actual-theme', "dark");
         actual_theme = "dark";
     }else{
-        document.documentElement.style.setProperty('--bs-border-color', light_color);
-        document.querySelector('.navbar').style.setProperty('--bs-navbar-toggler-border-color', light_color);
         document.getElementById("navbar-toggler-icon").style.backgroundImage = "url('../img/toggler-icon-bg.svg')";
 
-        let logo_img = getComputedStyle(document.documentElement).getPropertyValue('--light-logo-img').replace(/"/g, '');;
+        let logo_img = getComputedStyle(document.documentElement).getPropertyValue('--light-logo-img').replace(/"/g, '');
         document.getElementById("logo_img").src = logo_img;
+
+        document.body.style.background = "white";
+
+        document.documentElement.style.setProperty('--actual-hovered-links-color', "black");
+        document.documentElement.style.setProperty('--actual-color-one', dark_color);
+        document.documentElement.style.setProperty('--actual-color-two', light_color);
+        document.documentElement.style.setProperty('--actual-color-three', white_color);
+        document.documentElement.style.setProperty('--actual-non-active-links-color', dark_text_color);
 
         document.documentElement.style.setProperty('--actual-theme', "light");
         actual_theme = "light";
