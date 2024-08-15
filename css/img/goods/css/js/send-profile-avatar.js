161d9d2c681dcb0ef66e4cb29be4360f9ef59e32
bambaleylaorigin <sunmalygin@gmail.com>
2024-08-15 22:44:53 +0700

    Delete css/img/goods/css directory

--- css/img/goods/css/js/send-profile-avatar.js
+++
@@ -1,5 +0,0 @@
-const $img_span = document.querySelector(".submit-file-button");
-$img_span.addEventListener('change', function (event){
-    const form = document.getElementById('Form');
-    form.submit();
-});
